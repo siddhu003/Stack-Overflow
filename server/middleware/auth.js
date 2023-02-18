a2e9bc4dcb8a03fe3c61875b8e8517239e7952d6
A.Siddharth <120cs0033@iiitk.ac.in>
2023-02-18 10:47:45 +0530

    first commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken'
  const auth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1]
-        let decodeData = jwt.verify(token, "test")
+        //let decodeData = jwt.verify(token, "test")
+        let decodeData = jwt.verify(token, process.env.JWT_SECRET)
         req.userId = decodeData?.id 
         next()
     } catch (error) {
